c448a67679ff1ee85d06a78f471bd1873e7e8ee9
johnrhen <john.r.hendric@gmail.com>
2019-12-03 12:02:47 -0800

    Progress on adapter

--- app/src/main/java/edu/uw/eang16/wakey/MainActivity.kt
+++ app/src/main/java/edu/uw/eang16/wakey/MainActivity.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ArrayAdapter
+import android.widget.Switch
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import java.io.File
 import java.io.FileReader
@@ -37,21 +37,35 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+private class ViewHolder(var time: TextView? = null, var active: Switch? = null, var days: ConstraintLayout? = null)
 enum class Task { Shake, Scanner, Math, None }
 
 // Todo: Read all information unique to a single alarm
 data class AlarmData(var id: Int, var day: String, var time: String, var task: Task, var ringtone: String,
-                     var volume: Int, var vibration: Int, var snooze: Int, var limit: Int)
+                     var volume: Int, var vibration: Int, var snooze: Int, var limit: Int, var active: Boolean)
 
 class ForecastAdapter(context: Context, objects: List<AlarmData>):
     ArrayAdapter<AlarmData>(context, R.layout.alarm_list, objects) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
         val alarm = getItem(position)!!
-        var holder: RecyclerView.ViewHolder
+        var holder: ViewHolder
 
-        if (convertView == null) {
-
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.alarm_list, parent, false)
+            holder = ViewHolder()
+            holder.time = view.findViewById(R.id.alarmTime)
+            holder.active = view.findViewById(R.id.switchBtn)
+            holder.days = view.findViewById(R.id.days)
+            view.tag = holder
+        } else {
+            holder = view.tag as ViewHolder
         }
+        holder.time!!.text = alarm.time
+        //Todo: set the day color to be proper
+        holder.active!!.isChecked = alarm.active
+
+        return view!!
     }
 }
