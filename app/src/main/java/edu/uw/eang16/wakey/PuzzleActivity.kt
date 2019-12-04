baf3ed8e5cc8ef50e286e68496aa191bc7169d47
peeras-1766951 <peeras@uw.edu>
2019-12-04 12:33:32 -0800

    random answer and key

--- app/src/main/java/edu/uw/eang16/wakey/PuzzleActivity.kt
+++ app/src/main/java/edu/uw/eang16/wakey/PuzzleActivity.kt
@@ -3,25 +3,25 @@ package edu.uw.eang16.wakey
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_math_solver.*
 import kotlinx.android.synthetic.main.activity_math_solver.snooze
 import kotlinx.android.synthetic.main.activity_puzzle_solver.*
 import java.util.*
+import kotlin.concurrent.schedule
+import kotlin.random.Random
 
 class PuzzleActivity: AppCompatActivity() {
+
+    var key = mutableListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_puzzle_solver)
 
-        val TRUE = " "
-        val FALSE = ""
 
-        val answer = arrayOf(TRUE, FALSE, TRUE,
-            FALSE, TRUE, FALSE,
-            FALSE, FALSE, FALSE)
 
         val buttons = arrayOf(button, button2, button3,
                               button4, button5, button6,
@@ -39,6 +39,11 @@ class PuzzleActivity: AppCompatActivity() {
             }
         }
 
+        val answer = randomAnswer()
+        showAnswer(buttons)
+        Timer("SetUp", false).schedule(2000) {
+            clear(buttons)
+        }
 
         snooze.setOnClickListener {
             var check = true
@@ -51,10 +56,38 @@ class PuzzleActivity: AppCompatActivity() {
                 Toast.makeText(this, "Correct pattern!", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "Incorrect pattern. Try again!", Toast.LENGTH_SHORT).show()
+                clear(buttons)
             }
         }
+    }
 
+    fun randomAnswer(): Array<String> {
 
+        val TRUE = " "
+        val FALSE = ""
 
+        val pattern = arrayOf(FALSE, FALSE, FALSE,
+            FALSE, FALSE, FALSE,
+            FALSE, FALSE, FALSE)
+
+        for(i in 0..2) {
+            val index = Random.nextInt(8)
+            pattern[index] = TRUE
+            key.add(index)
+        }
+
+        return pattern
+    }
+
+    fun showAnswer(btns: Array<Button>) {
+        for (key in key) {
+            btns[key].background = getResources().getDrawable(R.drawable.circle_selected)
+        }
+    }
+
+    fun clear(btns: Array<Button>) {
+        for (btn in btns) {
+            btn.background = getResources().getDrawable(R.drawable.circle)
+        }
     }
 }
