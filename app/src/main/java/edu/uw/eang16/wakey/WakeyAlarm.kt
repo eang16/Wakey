b8692ebb66bf15e10ca8695cc55733daffadd8b9
johnrhen <john.r.hendric@gmail.com>
2019-12-04 13:09:35 -0800

    Create music player for WakeySoundService interface

--- app/src/main/java/edu/uw/eang16/wakey/WakeyAlarm.kt
+++ app/src/main/java/edu/uw/eang16/wakey/WakeyAlarm.kt
@@ -1,17 +1,36 @@
 package edu.uw.eang16.wakey
 
+import android.content.Context
+import android.content.Intent
+
+// Remember to call superfunctions on all the below!
 interface WakeyAlarm {
     // Uses the information from AlarmData to set off the alarm. Should be a part of onCreate.
-    fun startAlarm(alarmData: AlarmData)
+    fun startAlarm(alarmData: AlarmData, context: Context) {
+        context.startService(Intent(context.applicationContext, WakeySoundService::class.java).apply {
+            putExtra("data", alarmData)
+        })
+    }
 
     // When the alarm wakes up after a snooze, this resumes the ringtone and alarm
-    fun resumeAlarm(alarmData: AlarmData)
+    fun resumeAlarm(alarmData: AlarmData, context: Context) {
+        context.startService(Intent(context.applicationContext, WakeySoundService::class.java).apply {
+            putExtra("data", alarmData)
+        })
+    }
 
     // Keeps track of how many snoozes are available and disables snooze if none are left
-    fun snoozeAlarm(alarmData: AlarmData) {
+    fun snoozeAlarm(alarmData: AlarmData, context: Context) {
+        context.stopService(Intent(context.applicationContext, WakeySoundService::class.java).apply {
+            putExtra("data", alarmData)
+        })
         val snoozeTimeInMinutes = SNOOZE_TIMES[alarmData.snooze]
     }
 
     // Upon successfully completing the task, stops the alarm
-    fun stopAlarm()
+    fun stopAlarm(alarmData: AlarmData, context: Context) {
+        context.stopService(Intent(context.applicationContext, WakeySoundService::class.java).apply {
+            putExtra("data", alarmData)
+        })
+    }
 }
---
+++ app/src/main/java/edu/uw/eang16/wakey/WakeySoundService.kt
@@ -0,0 +1,81 @@
+package edu.uw.eang16.wakey
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+class WakeySoundService: Service() {
+    lateinit var data: AlarmData
+    lateinit var ringtone: Ringtone
+
+    companion object {
+        private val CHANNEL_ID = "Wakey Alarm Clock Notification Channel"
+        private var mediaPlayer: MediaPlayer? = null
+        private var notifChannel: NotificationChannel? = null
+        private var notifManager: NotificationManager? = null
+
+        private fun getNC(): NotificationChannel? {
+            if (notifChannel == null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val name = CHANNEL_ID
+                val descriptionText = "The Wakey App"
+                val importance = NotificationManager.IMPORTANCE_HIGH
+                notifChannel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                    description = descriptionText
+                }
+            }
+            return notifChannel
+        }
+
+        private fun getNM(context: Context): NotificationManager {
+            if (notifManager == null) {
+                notifManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            }
+            return notifManager!!
+        }
+
+        private fun buildNotification(context: Context): Notification {
+            val channel = getNC()
+            val manager = getNM(context)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                manager.createNotificationChannel(channel!!)
+            }
+
+            //TODO: Make an intent which returns to the currently playing alarm (singleTop?)
+            var builder = NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Wakey")
+                .setContentText("Currently sounding an alarm!")
+                .setAutoCancel(true)
+                .build()
+
+            return builder
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        data = intent!!.getParcelableExtra("data") as AlarmData
+        startForeground(data.id.toInt(), buildNotification(applicationContext))
+        ringtone = RingtoneManager.getRingtone(applicationContext, data.ringtone)
+        ringtone.play()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        stopForeground(true)
+        ringtone.stop()
+        super.onDestroy()
+    }
+}
