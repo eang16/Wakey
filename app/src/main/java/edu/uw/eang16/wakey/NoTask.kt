413686347278d04425491fd08f55b437a16ac764
johnrhen <john.r.hendric@gmail.com>
2019-12-04 13:21:25 -0800

    Remove useless tasks

--- app/src/main/java/edu/uw/eang16/wakey/NoTask.kt
+++ app/src/main/java/edu/uw/eang16/wakey/NoTask.kt
@@ -3,7 +3,7 @@ package edu.uw.eang16.wakey
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class NoTask : AppCompatActivity() {
+class NoTask : AppCompatActivity(), WakeyAlarm {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
