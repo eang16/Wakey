0ce0a6d63bed28aa0b17e11508cd5040b0d377d5
johnrhen <john.r.hendric@gmail.com>
2019-12-05 19:15:25 -0800

    MathSolver on lockscreen

--- app/src/main/java/edu/uw/eang16/wakey/MathSolver.kt
+++ app/src/main/java/edu/uw/eang16/wakey/MathSolver.kt
@@ -1,9 +1,12 @@
 package edu.uw.eang16.wakey
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_math_solver.*
 import kotlin.random.Random
@@ -33,11 +36,19 @@ class MathSolver: AppCompatActivity(), WakeyAlarm {
     val operator = listOf(Operator.PLUS, Operator.MINUS, Operator.MULTIPLY, Operator.DIVIDE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             setShowWhenLocked(true)
             setTurnScreenOn(true)
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager.requestDismissKeyguard(this, null)
+        } else {
+            window.addFlags(
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
         }
-        super.onCreate(savedInstanceState)
+        
         setContentView(R.layout.activity_math_solver)
         problem.text = makeProblem()
         val data = intent!!.getParcelableExtra("data") as AlarmData
